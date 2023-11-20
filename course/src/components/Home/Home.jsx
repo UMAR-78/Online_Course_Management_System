e5773bd030649cb7acfc6eb6ec1209be0a1b71fb
Mehak Aftab <mehakaftab324@gmail.com>
2023-11-21 02:14:26 +0500

    Correct Error:2                                                Import home.css to Home page to adjust footer position

--- course/src/components/Home/Home.jsx
+++ course/src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './home.css'; // Import your custom CSS file here
 import { Link } from 'react-router-dom';
 import { Button } from '@chakra-ui/react'; // Import Chakra UI Button
 
